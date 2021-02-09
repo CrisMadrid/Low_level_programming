0f169614bb8a120c59762d4fe8fbdaa5d75654ab
CrisMadrid <cambanne@gmail.com>
2021-02-09 17:11:08 +0000

    script that generates the assembly code of a C code and save it in an output file

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,18 +1,3 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    return (0);
-}
-julien@ubuntu:~/c/0x00$ ./2-assembler
-julien@ubuntu:~/c/0x00$ ls
-0-preprocessor  1-compiler  2-assembler c  main.c  main.s  Makefile
-julien@ubuntu:~/c/0x00$ cat main.s
     .file   "main.c"
     .text
     .globl  main
