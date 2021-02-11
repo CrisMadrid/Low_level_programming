3a30663911bde3cb454cc38e63ff3c4d37af44df
CrisMadrid <cambanne@gmail.com>
2021-02-11 01:29:56 +0000

    Functions, nested loops project

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+*main - main funtion
+*@c: var
+*_putchar - Entry point
+*
+*Return: Always 0 (Sucess)
+*/
+int _putchar(char c);
+int main(void)
+{
+_putchar(72);
+_putchar(111);
+_putchar(108);
+_putchar(98);
+_putchar(101);
+_putchar(114);
+_putchar(116);
+_putchar(111);
+_putchar(110);
+_putchar('\n');
+	return (0);
+}
